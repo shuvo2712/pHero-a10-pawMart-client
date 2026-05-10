bcc131a1583674055e7b2bdc39cdd63f6f306314
shuvo2712 <shuvo2712@gmail.com>
2026-05-11 04:37:50 +0600

    firebase key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBz2hX2mqma4kYRlvMd9_cEyEf8cADn5EE",
-  authDomain: "phero-a10-pawmart.firebaseapp.com",
-  projectId: "phero-a10-pawmart",
-  storageBucket: "phero-a10-pawmart.firebasestorage.app",
-  messagingSenderId: "997104889012",
-  appId: "1:997104889012:web:1453e57c1b4195d3b87be1"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 const app = initializeApp(firebaseConfig);
